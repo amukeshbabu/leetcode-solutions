dc74ac4dd18a70064e45641802a1b7960e80af80
Mukesh Babu A <amukeshbabu@gmail.com>
2026-09-03 14:03:15 +0530

    Auto-Sync: Solved Construct Uniform Parity Array II

--- 3876 - Construct Uniform Parity Array II/Solution.java
+++ 3876 - Construct Uniform Parity Array II/Solution.java
@@ -1,18 +1,41 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        int nums2[] = new int[n];
 
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2 == 0){
-                nums2[i] = nums1[i];
-            }else if(nums1[i]%2 != 0){
-                nums2[i] = nums1[i];
-            }else{
-                nums2[i] = nums1[i] - nums1[i-1];
-                return false;
+        boolean hasOdd = false;
+        boolean hasEven = false;
+
+        for (int num : nums1) {
+            if (num % 2 == 0) {
+                hasEven = true;
+            } else {
+                hasOdd = true;
             }
         }
-        return true;
+
+        // Already all same parity
+        if (!hasOdd || !hasEven) {
+            return true;
+        }
+
+        // Find the smallest number
+        int min = nums1[0];
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+        }
+
+        // If the smallest number is odd,
+        // every larger even number can become odd.
+        if (min % 2 == 1) {
+            return true;
+        }
+
+        // If the smallest number is even,
+        // every larger odd number can become odd,
+        // but even numbers cannot become odd using the minimum.
+        //
+        // Therefore, we need to check whether all even numbers
+        // can remain even.
+        return false;
     }
 }
