0d2ba8b3ec1ab1c42ee0bf9f30b30d871cb08ca4
Mukesh Babu A <amukeshbabu@gmail.com>
2026-09-16 20:34:44 +0530

    Auto-Sync: Solved Check Divisibility by Digit Sum and Product

---
+++ 3622 - Check Divisibility by Digit Sum and Product/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int t = n;
+        int sum = 0;
+        int product = 1;
+
+        while(t > 0){
+            int infinia = t % 10;
+
+            sum += infinia;
+            product *= infinia;
+
+            t /= 10;
+        }
+        return n % (sum + product) == 0;
+    }
+}
